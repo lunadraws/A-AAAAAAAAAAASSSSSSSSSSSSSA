d463d9e87fd70fa1b8e1c5c2d91ceaf4b6ed8984
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-04-19 15:01:52 +0200

    Update setanischedch.js

--- commands/setup/setanischedch.js
+++ commands/setup/setanischedch.js
@@ -5,7 +5,7 @@ module.exports = {
   aliases: [ 'setanischedulechannel', 'setanischedulech', 'setanischedchannel' ],
   guildOnly: true,
   adminOnly: true,
-  group: '**__Configuration__**',
+  group: 'setup',
   description: 'Where will i post the announcement for recently aired anime?',
   requiresDatabase: true,
   parameters: ['Channel ID/Mention'],
@@ -16,7 +16,7 @@ module.exports = {
   run: (client, message, [channel='']) => list.findById(message.guild.id, (err, doc) => {
 
     if (err){
-      return message.channel.send(`\`❌ [DATABASE_ERR]:\` La base de données a répondu avec une erreur: ${err.name}`);
+      return message.channel.send(`\`❌ [DATABASE_ERR]:\` The database responded with error: ${err.name}`);
     } else if (!doc){
       doc = new list({ _id: message.guild.id });
     };
@@ -25,18 +25,18 @@ module.exports = {
     channel = message.guild.channels.cache.get(channelID);
 
     if (!channel || !['text', 'news'].includes(channel.type)){
-      return message.channel.send(`\\❌ **${message.member.displayName}**, veuillez fournir un identifiant de chaîne ou une mention de chaîne valide.`);
+      return message.channel.send(`\\❌ **${message.member.displayName}**, please provide a valid channel ID or channel mention.`);
     } else if (!channel.permissionsFor(message.guild.me).has('SEND_MESSAGES')){
-      return message.channel.send(`\\❌ **${message.member.displayName}**, J'ai besoin que vous m'autorisiez à **Envoyer des messages** sur ${channel} et essayez à nouveau.`);
+      return message.channel.send(`\\❌ **${message.member.displayName}**, I need you to give me permission to **Send Messages** on ${channel} and try again.`);
     } else if (!channel.permissionsFor(message.guild.me).has('EMBED_LINKS')){
-      return message.channel.send(`\\❌ **${message.member.displayName}**, J'ai besoin que vous m'autorisiez à **intégrer des liens** sur ${channel} et essayez à nouveau.`);
+      return message.channel.send(`\\❌ **${message.member.displayName}**, I need you to give me permission to **Embed Links** on ${channel} and try again.`);
     };
 
     doc.channelID = channel.id;
     return doc.save()
     .then(() => {
 
-      return message.channel.send(`\\✔️ Définit avec succès le salon de notification de diffusion d'anime sur ${channel}!`)
-    }).catch(() => message.channel.send(`\`❌ [DATABASE_ERR]:\` Impossible d'enregistrer le document dans la base de données, veuillez réessayer plus tard!`))
+      return message.channel.send(`\\✔️ Successfully set the anime airing notification channel to ${channel}!`)
+    }).catch(() => message.channel.send(`\`❌ [DATABASE_ERR]:\` Unable to save the document to the database, please try again later!`))
   })
 };
