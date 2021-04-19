75ffd5545bdb0004b0572f8534755a04997742fd
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-04-19 19:21:08 +0200

    Update userVoted.js

--- events/userVoted.js
+++ events/userVoted.js
@@ -8,24 +8,24 @@ module.exports = (client, req, res) => profile.findById(req.vote.user, async (er
   const isWeekend = req.vote.isWeekend;
   const reward =  isWeekend ? 1500 : 750;
   const reason = [
-    'une erreur s\'est produite lors de l\'accès à votre profil à partir de la base de données!',
-    'vous ne vous êtes pas d\'abord inscrit au système économique. Utilisez la commande `register` pour vous inscrire et recevoir des récompenses de vote.'
+    'there was an error accessing your profile from the database!',
+    'you did not register to the economy system first. Use the command `register` to register and receive vote rewards.'
   ];
 
   if (err){
     return user?.send([
-      `\\❌ | **${user.tag}**, il semble que tu m'as voté sur top.gg, mais ${reason[0]}`,
-      `Si vous recevez ce message, veuillez contacter FloxYTB#9674 immédiatement. Préparez également une capture d'écran de ce message.`
+      `\\❌ | **${user.tag}**, seems like you voted me on top.gg, but ${reason[0]}`,
+      `Should you receive this message, please reach out to floxYTB#9674 immediately. Prepare also a screenshot of this message.`
     ].join('\n')).catch(() => {
-      return console.log(`[VOTE_EVENT]: Impossible d'envoyer le message à l'utilisateur ${req.vote.user}`);
+      return console.log(`[VOTE_EVENT]: Could not send message to user ${req.vote.user}`);
     });
   };
 
   if (!doc){
     return user?.send([
-      `\\❌ | **${user.tag}**, il semble que tu m'as voté sur top.gg, mais ${reason[1]}`
+      `\\❌ | **${user.tag}**, seems like you voted me on top.gg, but ${reason[1]}`
     ].join()).catch(() => {
-      return console.log(`[VOTE_EVENT]: Impossible d'envoyer le message à l'utilisateur ${req.vote.user}`);
+      return console.log(`[VOTE_EVENT]: Could not send message to user ${req.vote.user}`);
     });
   };
 
@@ -33,7 +33,7 @@ module.exports = (client, req, res) => profile.findById(req.vote.user, async (er
 
   if (doc.data.economy.wallet + reward > 5e4){
     overflow = true;
-    excess = doc.data.economy.wallet + amount - 5e4;
+    excess = doc.data.economy.wallet + reward - 5e4;
   };
 
   doc.data.economy.wallet += overflow ? reward - excess : reward;
@@ -41,15 +41,15 @@ module.exports = (client, req, res) => profile.findById(req.vote.user, async (er
   return doc.save()
   .then(() => {
     const message = [
-      `<a:animatedcheck:758316325025087500> | **${user.tag}**, Merci d'avoir voté!`,
-      `Tu as reçu **${text.commatize(reward)}** ${isWeekend ? '**(Récompense double week-end)**' : ''} des crédits en récompense!`,
-      overflow ? `\n⚠️Avertissement de débordement! Veuillez déposer une partie de votre compte dans votre **banque**. Vous avez seulement reçu ${reward - excess} pour celui-ci!` :'',
-      `Vous ne voulez pas être informé de chaque vote que vous faites? Utilisez la commande \`${client.prefix}togglevotenotif\` pour enable/disable vote notifications!\n(Ne vous empêche pas de recevoir des récompenses)`
+      `<a:animatedcheck:758316325025087500> | **${user.tag}**, Thanks for voting!`,
+      `You received **${text.commatize(reward)}** ${isWeekend ? '**(Double Weekend Reward)**' : ''} credits as a reward!`,
+      overflow ? `\n⚠️Overflow warning! Please deposit some of your account to your **bank**. You only received ${reward - excess} for this one!` :'',
+      `Don't want to get notified of every vote you make? Use the command \`${client.prefix}togglevotenotif\` to enable/disable vote notifications!\n(Does not prevent you from receiving rewards)`
     ].join('\n')
 
     if (doc.data.vote.notification){
       user?.send(message).catch(()=>{
-        return console.log(`[VOTE_EVENT]: Impossible d'envoyer le message à l'utilisateur ${req.vote.user}`);
+        return console.log(`[VOTE_EVENT]: Could not send message to user ${req.vote.user}`);
       });
     };
 
