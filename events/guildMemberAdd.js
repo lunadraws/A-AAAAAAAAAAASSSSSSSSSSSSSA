1de5ccd2888f8b35accacb204b7e450c455eb243
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-04-19 18:36:17 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,45 +1,42 @@
 const { MessageEmbed } = require('discord.js');
 const modifier = require(`${process.cwd()}/util/modifier`);
-const string = require(`${process.cwd()}/util/string`);
 
-module.exports = async ( client, member ) => {
+module.exports = async (client, member) => {
 
   const guildProfile = client.guildProfiles.get(member.guild.id);
 
-  if (!guildProfile.greeter.welcome.isEnabled){
+  if (!guildProfile.greeter.leaving.isEnabled){
     return;
-  } else if (!guildProfile.greeter.welcome.channel){
+  } else if (!guildProfile.greeter.leaving.channel) {
     return;
-  } else if (!member.guild.channels.cache.get(guildProfile.greeter.welcome.channel)){
+  } else if (!member.guild.channels.cache.get(guildProfile.greeter.leaving.channel)){
     return;
   } else {
     // Do nothing..
   };
-  
-  const welcome = guildProfile.greeter.welcome;
-  const type = welcome.type === 'msg' && !welcome.message ? 'default' : welcome.type;
+
+  const leaving = guildProfile.greeter.leaving;
+  const type = leaving.type === 'msg' && !leaving.message ? 'default' : leaving.type;
 
   if (type === 'default'){
-    return client.channels.cache.get(guildProfile.greeter.welcome.channel).send(
+    return client.channels.cache.get(guildProfile.greeter.leaving.channel).send(
       new MessageEmbed()
-      .setColor(`#42FF00`)
-      .setTitle(`${member.user.tag} a rejoint notre serveur!`)
+      .setColor('#42FF00')
+      .setTitle(`${member.user.tag} has left our server!`)
       .setThumbnail(member.user.displayAvatarURL({format: 'png', dynamic: true}))
-      .setDescription(`Bonjour ${member}, Bienvenue sur **${member.guild.name}**!\n\nTu es notre **${string.ordinalize(member.guild.memberCount)}** membre!`)
-      .setFooter(`Accueil des membres | ©️${new Date().getFullYear()} HorizonGame`)
+      .setDescription(`Byebye ${member}!! Sad to see you go!\n\nWe are back to **${member.guild.memberCount}** members!`)
+      .setFooter(`Leaving Member Announcer | ©️${new Date().getFullYear()} Horizongame`)
     );
   };
 
-  //if message was text, send the text
-   if (type === 'msg'){
-    const message = await modifier.modify(guildProfile.greeter.welcome.message, member);
-    return client.channels.cache.get(guildProfile.greeter.welcome.channel).send(message);
+  if (type === 'msg'){
+    const message = await modifier.modify(guildProfile.greeter.leaving.message, member)
+    return client.channels.cache.get(guildProfile.greeter.leaving.channel).send(message);
  };
 
-  //if message was embed
-  return client.channels.cache.get(guildProfile.greeter.welcome.channel).send(
-    new MessageEmbed(
-      JSON.parse(
-        await modifier.modify(JSON.stringify(guildProfile.greeter.welcome.embed), member)))
-  );
+ return client.channels.cache.get(guildProfile.greeter.leaving.channel).send(
+   new MessageEmbed(
+     JSON.parse(
+       await modifier.modify(JSON.stringify(guildProfile.greeter.leaving.embed), member)))
+ );
 };
