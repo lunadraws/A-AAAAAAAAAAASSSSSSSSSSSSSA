2c56666a397c9cc95513372e89a1fb6213f3431a
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-04-19 20:34:33 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -16,7 +16,7 @@ module.exports = (client, guild) => guilds.findById(guild.id, async (err, doc) =
   .then(owner => owner.tag)
   .catch(() => '<Unfetched Data>');
 
-  const logo = '<:Enter:794918219835637760>';
+  const logo = '<a:lovelove:767790365985341461>';
   const members = text.commatize(guild.memberCount);
   const message = `${logo} : **${members}** members, owned by **${owner}**`;
   //====================================================//
