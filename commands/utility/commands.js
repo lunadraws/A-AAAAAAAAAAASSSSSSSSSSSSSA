deb959429796ee5db6c05d451a543142fcb5fe93
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-04-20 16:35:57 +0200

    Add files via upload

---
+++ commands/utility/commands.js
@@ -0,0 +1,66 @@
+const { MessageEmbed } = require('discord.js');
+const text = require('../../util/string');
+
+module.exports = {
+  name: 'help',
+  aliases: [ 'cmd', 'command' ],
+  group: 'core',
+  description: 'Sends a list of all commands from each specific command groups',
+  clientPermissions: [ 'EMBED_LINKS' ],
+  parameters: [],
+  examples: [
+    'commands',
+    'cmd',
+    'command'
+  ],
+  run: (client, message) => {
+
+    return message.channel.send(
+      new MessageEmbed()
+      .setColor(message.guild.me.displayHexColor)
+      .setAuthor('Liste complète des commandes de HorizonGame!')
+      .setImage('https://cdn.discordapp.com/attachments/766973870350663730/814984024233345024/standard.gif')
+      .setTimestamp()
+      .addField(
+        '**__liens__**', 
+        '**[Invite moi](https://discordapp.com/oauth2/authorize?client_id=688407554904162365&scope=bot&permissions=2146958847) | ' +
+        '[Serveur de support](https://discord.gg/fz3TXnw) | ' +
+        '[Youtube](https://youtube.com/c/FloxYtbTuto)**'
+      )
+      .setFooter(`help | \©️${new Date().getFullYear()} HorizonGame`)
+      .addField(
+          '**__couleur__**',
+          "en maintenance")
+       .addField(
+        '__**modération**__',
+        "`addemoji`, `addroles`, `ban`, `clear`, `hackban`, `kick`, `lockdown`, `mute`, `nuke`, `resetchannel`, `resetroles`, `reponse`, `softban`, `softlockdown`, `softmute`, `unban`, `unmute`"
+       )
+       .addField(
+        '__**anime**__',
+        "`alprofile`, `anime`, `animeme`, `aniquote`, `anirandom`, `character`, `discover`, `hanime`, `malprofile`, `manga`, `mangarandom`, `nextairdate`, `schedule`, `seiyuu`, `upcoming`, `waifu`"
+       )
+       .addField(
+        '__**économie**__',
+        "`addcredits`, `bal`, `bank`, `beg`, `bet`, `creditslb`, `daily`, `deposit`, `find`, `game`, `leaderboard`, `nonxpchannels`, `profile`, `register`, `setbio`, `setbirthday`, `setcolor`, `tip`, `pay`, `withdraw`"
+       )
+       .addField(
+        '__**configuration**__',
+        "`disableanisched`, `goodbyetoggle`, `setanischedch`, `setgoodbyech`, `setgoodbyemsg`, `setmute`, `setprefix`, `setsuggestch`, `setwelcomech`, `setwelcomemsg`, `togglevotenotif`, `unwatch`, `userxpreset`, `watch`, `welcometoggle`, `xpenable`, `xpexcempt`, `xpreset`, `xptoggle`, `addbotchat`, `adddj`, `removedj`, `removebotchat`"
+       )
+       .addField(
+        '__**amusant**__',
+        "`advice`, `baka`, `birdfacts`, `blush`, `catfacts`, `comment`, `cry`, `dance`, `disgust`, `dogfacts`, `feed`, `fortune`, `happy`, `holdhands`, `horoscope`, `hug`, `invert`, `joke`, `kill`, `kiss`, `lick`, `meme`, `midfing`, `pandafacts`, `pat`, `poke`, `pokemon`, `rate`, `respect`, `reverse`, `roll`, `slap`, `sleep`, `smile`, `smug`, `suicide`, `tickle`, `triggered`, `wave`, `wink`"
+       )
+       .addField(
+        '__**utile**__',
+        "`sleeptimer`, `embed`, `feedback`, `avatar`, `reportbug`, `define`, `emoji`, `jisho`, `listrole`, `reddit`, `steam`, `suggest`, `watching`, `dj`, `invite`, `ping`, `uptime`"
+       )
+       .addField(
+        '__**musique**__',
+        "`addrelated`, `autoplay`, `playlist`, `clearqueue`, `custom`, `forward`, `grab`, `join`, `jump`, `loop`, `lyrics`, `moveme`, `mycustom`, `nowplaying`, `pause`, `play`, `playsc`, `playskip`, `radio`, `removetrack`, `replay`, `resume`, `rewind`, `search`, `searchrelated`, `searchsc`, `seek`, `shuffle`, `skip`, `status`, `stop`, `volume`"
+       )
+       
+       
+    );
+  }
+};
