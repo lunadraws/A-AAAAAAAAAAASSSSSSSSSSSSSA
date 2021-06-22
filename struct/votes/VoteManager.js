c84af687c84549a6fbb55780054bd89ffdf9b307
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-06-22 14:20:16 +0200

    Update VoteManager.js

--- struct/votes/VoteManager.js
+++ struct/votes/VoteManager.js
@@ -1,64 +1,6 @@
 const { URLSearchParams } = require('url');
-const { Api, Webhook }    = require('@top-gg/sdk');
-const fetch = require('node-fetch');
-
-class TopGG{
-  constructor(client){
-    Object.defineProperty(this, 'client', { value: client });
-
-    this.api     = new Api(process.env.TOP_GG_AUTH);
-    this.webhook = new Webhook('MaiBestWaifu');
-
-    // This only need to be run once, run only on shard 0
-    if (!this.client.shard.ids[0] !== 0){
-      return;
-    };
-
-    const application  = require('express')();
-    const middleware   = this.webhook.middleware;
-    const emitToClient = (req, res) => { this.client.emit('userVoted', req, res); res?.status(200)?.send('OK')};
-
-    application.post('/dblwebhook', middleware(), (req, res) => emitToClient(req, res));
-    application.listen(process.env.TOP_GG_PORT || 1200);
-
-    require('topgg-autoposter')(process.env.TOP_GG_AUTH, client);
-  };
-};
-
-class Dbl{
-  constructor(client, options = {}){
-    Object.defineProperty(this, 'client', { value: client });
-    this.webhook = {
-      url : typeof options.url === 'string' ? options.url : null
-    };
-  };
-  async post(){
-    const params  = new URLSearchParams();
-    const baseURI = 'https://discordbotlist.com/api/v1';
-
-    for (const parameter of ['guilds', 'users']){
-      params.append(parameter, this.client[parameter].cache.size);
-    };
-    params.append('shard_id', this.client.shard.ids[0]);
-
-    const options = {
-      method : 'post',
-      headers: { 'Authorization': process.env.DBL_AUTH },
-      path   : { 'id': this.client.user.id },
-      body   : params
-    };
-
-    const poststats = await fetch(`${baseURI}/bots/${this.client.user.id}/stats`, options);
-
-    if (poststats.status !== 200){
-      console.log(`\x1b[35m[SHARD_${this.client.shard.ids[0]}] \x1b[33m[MAI_VOTEMANAGER]\x1b[0m: Error on DBL poststats: ${poststats.status}`);
-    } else {
-      const json = await poststats.json().catch(err => err);
-      this.client.emit('TOPGGPostStatistics', json);
-    };
-    return this;
-  };
-};
+const Top = require('@top-gg/sdk');
+const Dbl = require(`${process.cwd()}/struct/votes/DBL`);
 
 module.exports = class VoteManager{
   constructor(client){
@@ -70,20 +12,48 @@ module.exports = class VoteManager{
      */
     Object.defineProperty(this, 'client', { value: client })
 
-    this.dbl    = null;
+    this.dbl = null;
     this.top_gg = null;
-
-    if (!['688407554904162365'].includes(client.user.id)){
-      return this;
-    };
 
     if (typeof process.env.DBL_AUTH === 'string'){
       this.dbl = new Dbl(client);
-      this.client.loop(() => this.dbl.post(), 18e5);
+    } else {
+      // Do nothing..
     };
 
     if (typeof process.env.TOP_GG_AUTH === 'string'){
-      this.top_gg = new TopGG(client);
+
+      const app = require('express')();
+
+      this.top_gg = {
+        api: new Top.Api(process.env.TOP_GG_AUTH),
+        webhook: new Top.Webhook('MaiBestWaifu')
+      };
+
+      app.post('/dblwebhook', this.top_gg.webhook.middleware(), (req ,res) => {
+        this.client.emit('userVoted', req, res);
+        
+        res?.status(200)?.send('OK');
+      });
+
+      app.listen(21753);
+    } else {
+      // Do nothing..
     };
+  };
+
+  _post(){
+    const serverCount = this.client.guilds.cache.size;
+    this.dbl?.post();
+    this.top_gg?.api.postStats({ serverCount });
+  };
+
+  init(loop){
+    if (loop){
+      this._post(); setInterval(() =>  this._post() , 1800000);
+    } else {
+      this._post();
+    };
+    return;
   };
 };
