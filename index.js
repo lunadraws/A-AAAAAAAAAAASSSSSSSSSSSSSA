f780ff4129a4d824d3e23665a2ffb1c27fbe477b
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-10 14:56:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const options = {
   bypass: true,
   log: true,
   paths: [
-    'anime', 'fun', 'moderation', 'setup', 'économie', 'utility'
+    'anime', 'fun', 'setup', 'économie', 'utility'
   ]
 };
 
