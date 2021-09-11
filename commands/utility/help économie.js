a55a8a5a2034080e6ba145eba411760a2e45654f
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-11 21:16:43 +0200

    Update help économie.js

--- commands/utility/help économie.js
+++ commands/utility/help économie.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 const text = require('../../util/string');
 
 module.exports = {
-  name: 'help-utile',
+  name: 'help-eco',
   group: 'core',
   description: 'Sends a list of all commands from each specific command groups',
   clientPermissions: [ 'EMBED_LINKS' ],
