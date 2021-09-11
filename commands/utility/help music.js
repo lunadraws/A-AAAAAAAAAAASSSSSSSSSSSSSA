70761d89752670f5f27340f9bd6105d5b3d7d5cb
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-11 20:59:13 +0200

    Update help music.js

--- commands/utility/help music.js
+++ commands/utility/help music.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require('discord.js');
 const text = require('../../util/string');
 
 module.exports = {
-  name: 'help music',
-  aliases: [ 'help musics', 'help musique' ],
+  name: 'help',
+  aliases: [ 'help cmd', 'help command' ],
   group: 'core',
   description: 'Sends a list of all commands from each specific command groups',
   clientPermissions: [ 'EMBED_LINKS' ],
@@ -23,15 +23,7 @@ module.exports = {
       .setTimestamp()
       
       .setFooter(`help | \©️${new Date().getFullYear()} HorizonGame`)
-      .addField(
-        '<:valide:885075251371835402>┇**__liens__**', 
-        '> **[Invite moi](https://discordapp.com/oauth2/authorize?client_id=688407554904162365&scope=bot&permissions=2146958847) | ' +
-        '[Support](https://discord.gg/5qbAGCykRd) | ' +
-        '[Site web](https://zbdfd.ml/) | ' +
-        '[Top.gg](https://top.gg/bot/688407554904162365/vote) | ' +
-        '[Youtube](https://youtube.com/c/NettleYTBBDFD)**'
-      )
-       
+      
        .addField(
         '<:music:885078247954595870>』__**musique**__',
         "> `back`, `clearqueue`, `dj-system`, `loop`, `lyrics`, `np`, `pause`, `play`, `queue`, `remove`, `resume`, `seek`, `shuffle`, `skip`, `stop`, `volume`"
