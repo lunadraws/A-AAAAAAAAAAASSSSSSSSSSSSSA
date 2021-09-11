93615aed8e07daba0fdc830b8e3452d5bf53df11
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-11 21:09:26 +0200

    Update help utile.js

--- commands/utility/help utile.js
+++ commands/utility/help utile.js
@@ -2,8 +2,7 @@ const { MessageEmbed } = require('discord.js');
 const text = require('../../util/string');
 
 module.exports = {
-  name: 'help 2',
-  aliases: [ 'help cmd 2', 'help command 2' ],
+  name: 'help',
   group: 'core',
   description: 'Sends a list of all commands from each specific command groups',
   clientPermissions: [ 'EMBED_LINKS' ],
