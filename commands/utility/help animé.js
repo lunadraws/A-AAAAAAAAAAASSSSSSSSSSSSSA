3fa4f8a8b8ed89620b1c76b62ed8bdf90a06c4df
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-11 20:56:34 +0200

    Update help animé.js

--- commands/utility/help animé.js
+++ commands/utility/help animé.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require('discord.js');
 const text = require('../../util/string');
 
 module.exports = {
-  name: 'help animé',
-  aliases: [ 'help anime', 'help animes' ],
+  name: 'help',
+  aliases: [ 'help cmd', 'help command' ],
   group: 'core',
   description: 'Sends a list of all commands from each specific command groups',
   clientPermissions: [ 'EMBED_LINKS' ],
