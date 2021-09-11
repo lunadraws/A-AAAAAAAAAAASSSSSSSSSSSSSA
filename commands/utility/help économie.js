333ea3d152355151b3e7be74764ad65a2d0c4126
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-11 21:01:18 +0200

    Update help économie.js

--- commands/utility/help économie.js
+++ commands/utility/help économie.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require('discord.js');
 const text = require('../../util/string');
 
 module.exports = {
-  name: 'help économie',
-  aliases: [ 'help eco', 'help economie' ],
+  name: 'help',
+  aliases: [ 'help cmd', 'help command' ],
   group: 'core',
   description: 'Sends a list of all commands from each specific command groups',
   clientPermissions: [ 'EMBED_LINKS' ],
@@ -23,15 +23,7 @@ module.exports = {
       .setTimestamp()
       
       .setFooter(`help | \©️${new Date().getFullYear()} HorizonGame`)
-      .addField(
-        '<:valide:885075251371835402>┇**__liens__**', 
-        '> **[Invite moi](https://discordapp.com/oauth2/authorize?client_id=688407554904162365&scope=bot&permissions=2146958847) | ' +
-        '[Support](https://discord.gg/5qbAGCykRd) | ' +
-        '[Site web](https://zbdfd.ml/) | ' +
-        '[Top.gg](https://top.gg/bot/688407554904162365/vote) | ' +
-        '[Youtube](https://youtube.com/c/NettleYTBBDFD)**'
-      )
-       
+      
        .addField(
         '<:money:885078204476456980>』__**économie**__',
         "> `addcredits`, `bal`, `bank`, `beg`, `bet`, `creditslb`, `daily`, `deposit`, `find`, `game`, `register`, `pay`, `withdraw`"
