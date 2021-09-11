89119c473c25d94bb422d30fa88bc821dd84706c
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-11 19:44:29 +0200

    Update commands.js

--- commands/utility/commands.js
+++ commands/utility/commands.js
@@ -23,11 +23,19 @@ module.exports = {
       .setTimestamp()
       
       .setFooter(`help | \©️${new Date().getFullYear()} HorizonGame`)
-        .addField(
-        '🚫┇__**modération**__',
-        "> `setnickname`, `slowmode`, `addemoji`, `addroles`, `ban`, `clear`, `hackban`, `kick`, `lockdown`, `mute`, `nuke`, `resetchannel`, `resetroles`, `reponse`, `softban`, `softlockdown`, `softmute`, `unban`, `unmute`"
+       .addField(
+        '🔍┇__**invite-managers**__',
+        "> `addbonus`, `removebonus`, `removeinvites`, `restoreinvites`, `sync-invites`, `config`, `configdmjoin`, `configjoin`, `configleave`, `setdmjoin`, `setjoin`, `setleave`, `testdmjoin`, `testjoin`, `testleave`, `invite`, `joinstats`, `topinvite`"
        )
        .addField(
+         'Moserations',
+         ">'ban-id', 'ban', 'banlist', 'case', 'cases-list', 'clear-warns', 'clear', 'delete-case', 'gend', 'greroll', 'gstart', 'infractions', 'kick', 'lock', 'massban', 'mod-logs', 'mute', 'nuke', 'massrole', 'roles', 'slowmode', 'softban', 'sugg-accept', 'sugg-delete', 'sugg-refuse', 'ticket-add', 'ticket-close', 'unban', 'unbanall', 'unlock', 'unmute', 'warn'"
+       )
+      .addField(
+         'anti raid',
+         ">anti-dc, anti-pub, anti-caps, anti-mass-mentions, anti-spam, antiraid-logs, check-users, ignoreds, protect, verification, backup'"
+       )
+         .addField(
         '✏️┇__**anime**__',
         "> `alprofile`, `anime`, `animeme`, `aniquote`, `anirandom`, `character`, `discover`, `hanime`, `malprofile`, `manga`, `mangarandom`, `nextairdate`, `schedule`, `seiyuu`, `upcoming`, `waifu`"
        )
@@ -37,24 +45,24 @@ module.exports = {
        )
        .addField(
         '⚙️┇__**configuration**__',
-        "> `setlogchannel`, `disableanisched`, `goodbyetoggle`, `setanischedch`, `setgoodbyech`, `setgoodbyemsg`, `setmute`, `setsuggestch`, `setwelcomech`, `setwelcomemsg`, `togglevotenotif`, `unwatch`, `userxpreset`, `watch`, `welcometoggle`, `xpenable`, `xpexcempt`, `xpreset`, `xptoggle`"
+        "> auto-music, autoping, auto-publish, auto-reactions, auto-responders, autonick, autorole, boost-message, botautonick, botautorole, chatbot, count-channel, glogs, language, counter, rolereact, set-sugg, setcolor, set-logs, setprefix, tempvoc, ticket-system, youtube-alerts, `setlogchannel`, `disableanisched`, `goodbyetoggle`, `setanischedch`, `setgoodbyech`, `setgoodbyemsg`, `setmute`, `setsuggestch`, `setwelcomech`, `setwelcomemsg`, `togglevotenotif`, `unwatch`, `userxpreset`, `watch`, `welcometoggle`, `xpenable`, `xpexcempt`, `xpreset`, `xptoggle`"
        )
        .addField(
         '😆┇__**amusant**__',
-        "> `youtube`, `yesno`, `trumptweet`, `trivia`, `translate`, `topics`, `thouart`, `solotrivia`, `shibe`, `say`, `rps`, `minesweeper`, `math`, `geometrydashsay`, `fox`, `emojify`, `duck`, `dog`, `corona`, `coinflip`, `cat`, `bird`, `8ball`, `advice`, `baka`, `birdfacts`, `blush`, `catfacts`, `comment`, `cry`, `dance`, `disgust`, `dogfacts`, `feed`, `fortune`, `happy`, `holdhands`, `horoscope`, `hug`, `invert`, `joke`, `kill`, `kiss`, `lick`, `meme`, `midfing`, `pandafacts`, `pat`, `poke`, `pokemon`, `rate`, `respect`, `reverse`, `roll`, `slap`, `sleep`, `smile`, `smug`, `suicide`, `tickle`, `triggered`, `wave`, `wink`"
+        "> quiz, numberfacts, create-quiz, `8ball`, `advice`, `baka`, `birdfacts`, `blush`, `catfacts`, `comment`, `cry`, `dance`, `disgust`, `dogfacts`, `feed`, `fortune`, `happy`, `holdhands`, `horoscope`, `hug`, `invert`, `joke`, `kill`, `kiss`, `lick`, `meme`, `midfing`, `pandafacts`, `pat`, `poke`, `pokemon`, `rate`, `respect`, `reverse`, `roll`, `slap`, `sleep`, `smile`, `smug`, `suicide`, `tickle`, `triggered`, `wave`, `wink`"
        )
        .addField(
         '🔑┇__**utile**__',
-        "> `userinfo`, `stats`, `serverinfo`, `servericon`, `roleinfo`, `permissions`, `members`, `findid`, `emojis`, `channelinfo`, `botinfo`, `embed`, `avis`, `avatar`, `reportbug`, `define`, `emoji`, `jisho`, `listrole`, `reddit`, `steam`, `suggest`, `watching`, `ping`"
+        "> birthdays-list, calculation, holidays, addemoji, admins-list, avatar, botinfo, color, deleteemoji, discrim, help, permissions, ping, premium, remind, restrictemoji, roleinfo, serverinfo, snipe, suggest, tiktok, translate, uptime, userinfo"
        )
        .addField(
         '🎵┇__**musique**__',
-        "> `bassboost`, `clearsong`, `disconnect`, `grab`, `loop`, `loopqueue`, `lyrics`, `nowplaying`, `pause`, `play`, `queue`, `remove`, `resume`, `search`, `seek`, `shuffle`, `skip`, `skipto`, `volume`, `joinytb`"
+        "> back, clearqueue, dj-system, loop, lyrics, np, pause, play, queue, remove, resume, seek, shuffle, skip, stop, volume"
        )
        .addField(
-        '🔍┇__**invite-managers**__',
-        "> `addbonus`, `removebonus`, `removeinvites`, `restoreinvites`, `sync-invites`, `config`, `configdmjoin`, `configjoin`, `configleave`, `setdmjoin`, `setjoin`, `setleave`, `testdmjoin`, `testjoin`, `testleave`, `invite`, `joinstats`, `topinvite`"
-       )
+         'levels',
+         "> addlevel, addxp, leaderboard, rank, remove-rank, reset-levels, add-rank, ranks"
+         )
        .addField(
         '🖇️┇**__liens__**', 
         '> **[Invite moi](https://discordapp.com/oauth2/authorize?client_id=688407554904162365&scope=bot&permissions=2146958847) | ' +
