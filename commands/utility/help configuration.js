569e2be1052bd5ad400db8770144316207a6c6b9
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-11 20:57:36 +0200

    Update help configuration.js

--- commands/utility/help configuration.js
+++ commands/utility/help configuration.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require('discord.js');
 const text = require('../../util/string');
 
 module.exports = {
-  name: 'help configuration',
-  aliases: [ 'help config', 'help configs' ],
+  name: 'help',
+  aliases: [ 'help cmd', 'help command' ],
   group: 'core',
   description: 'Sends a list of all commands from each specific command groups',
   clientPermissions: [ 'EMBED_LINKS' ],
@@ -23,15 +23,7 @@ module.exports = {
       .setTimestamp()
       
       .setFooter(`help | \©️${new Date().getFullYear()} HorizonGame`)
-      .addField(
-        '<:valide:885075251371835402>┇**__liens__**', 
-        '> **[Invite moi](https://discordapp.com/oauth2/authorize?client_id=688407554904162365&scope=bot&permissions=2146958847) | ' +
-        '[Support](https://discord.gg/5qbAGCykRd) | ' +
-        '[Site web](https://zbdfd.ml/) | ' +
-        '[Top.gg](https://top.gg/bot/688407554904162365/vote) | ' +
-        '[Youtube](https://youtube.com/c/NettleYTBBDFD)**'
-      )
-       
+      
        .addField(
         '<:config:885078094426279977>』__**configuration**__',
         "> `auto-music`, `autoping`, `auto-publish`, `auto-reactions`, `auto-responders`, `autonick`, `autorole`, `boost-message`, `botautonick`, `botautorole`, `chatbot`, `count-channel`, `glogs`, `language`, `counter`, `rolereact`, `set-sugg`, `set-logs`, `setprefix`, `tempvoc`, `ticket-system`, `youtube-alerts`, `disableanisched`, `setanischedch`, `unwatch`, `watch`"
