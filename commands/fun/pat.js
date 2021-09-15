55015c114b32f68cfdbbbc76042db01493d6840e
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-15 10:59:06 +0200

    Update pat.js

--- commands/fun/pat.js
+++ commands/fun/pat.js
@@ -18,7 +18,7 @@ module.exports = {
 
     const url = client.images.pat();
     const embed = new MessageEmbed()
-    .setColor(message.guild.me.displayHexColor)
+    .setColor('#3A871F')
     .setImage(url)
     .setFooter(`Pat | \©️${new Date().getFullYear()} Horizongame`);
 
