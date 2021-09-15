40f90044c5f69dcf443e8703776bc9437565d86d
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-15 11:03:11 +0200

    Update smug.js

--- commands/fun/smug.js
+++ commands/fun/smug.js
@@ -16,7 +16,7 @@ module.exports = {
     return message.channel.send(
       new MessageEmbed()
       .setDescription(`${message.author} smugs.`)
-      .setColor(message.guild.me.displayHexColor)
+      .setColor('#3A871F')
       .setImage(client.images.smug())
       .setFooter(`Smug | \©️${new Date().getFullYear()} horizonGame`)
     );
