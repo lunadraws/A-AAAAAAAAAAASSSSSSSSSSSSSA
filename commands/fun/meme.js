bbf4ee6647bc3c9069d3ddd180e4609f0a72f8b0
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-15 10:57:55 +0200

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -23,7 +23,7 @@ module.exports = {
 
     return message.channel.send(
       new MessageEmbed()
-      .setColor(message.guild.me.displayHexColor)
+      .setColor('#3A871F')
       .setImage(data.url)
       .setAuthor(data.title, null, data.postLink)
       .setFooter(`${data.subreddit}:Meme | \©️${new Date().getFullYear()} Horizongame`)
