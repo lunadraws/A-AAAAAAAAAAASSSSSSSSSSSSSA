67e442bf9cf3039840a2007596125a939cd846e8
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-15 17:54:23 +0200

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -28,8 +28,8 @@ module.exports = {
          "> `help-mod`, `help-eco`, `help-anime`, `help-antiraid`, `help-invite`, `help-level`, `help-music`, `help-fun`, `help-utile`, `help-config`"
        )
       .addField(
-         '<:search:885115865417261066>』**__Informations__**',
-         "[Invite moi](https://discordapp.com/oauth2/authorize?client_id=884131896919994458&scope=bot&permissions=2146958847) | [Support](https://discord.gg/eCDp4jRvWu) | [Top.gg](https://top.gg/bot/688407554904162365/vote) | [z-bdfd](https://zbdfd.ml/dc) | [Youtube](https://youtube.com/c/NettleYTBBDFD)"
+         '<:level:885078162613108746>』**__Liens__**',
+         "[Invite moi](https://discordapp.com/oauth2/authorize?client_id=884131896919994458&scope=bot&permissions=2146958847) | [Support](https://discord.gg/eCDp4jRvWu) | [Top.gg](https://top.gg/bot/688407554904162365/vote) | [Z-bdfd](https://zbdfd.ml/dc) | [Youtube](https://youtube.com/c/NettleYTBBDFD)"
        )
 
          
