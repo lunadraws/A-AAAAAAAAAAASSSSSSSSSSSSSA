1a0a0bc07ca0c7d74980a4710c2bcded1fea1735
lunadraws <68550443+lunadraws@users.noreply.github.com>
2021-09-26 23:47:23 +0200

    Update Profile.js

--- struct/guilds/Profile.js
+++ struct/guilds/Profile.js
@@ -83,7 +83,20 @@ module.exports = class GuildProfile{
       }
     };
 
-    
+    this.xp = {
+
+      /**
+      * Whether the xp system is active on the guild
+      * @type {Boolean}
+      */
+      isActive: data.xp.isActive,
+
+      /**
+      * The channel IDs to blacklist from xp system.
+      * @type {string[]}
+      */
+      exceptions: data.xp.exceptions
+    };
 
     /**
     * Role IDs bound to specific bot function.
